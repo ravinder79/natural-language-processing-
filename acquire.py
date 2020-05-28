079a5e29a476d2c47d68d3deb258b088c68817d2
Ravinder Singh <ravinder79@gmail.com>
2020-05-28 16:16:51 -0500

    cleaned up

--- acquire.py
+++ acquire.py
@@ -27,29 +27,30 @@ def acquire_codeup_blog(url):
 def get_news_articles(urls):
     list = []
     for url in urls:
-        list.append(get_news_items(url))
-    return list
-
-def get_news_items(url):
-    url = url
-    headers = {'User-Agent': 'Codeup Data Science'}
-    response = get(url, headers=headers)
-    soup = BeautifulSoup(response.text)
-    headlines = soup.find_all(attrs={"itemprop": "headline"})
-    bodies = soup.find_all(attrs={"itemprop": "articleBody"})
-    category = re.search(r'([^\/]+$)', url)[0]
-    
-    #get list of headlines, content and categories
-    for i in range(0, len(headlines)):
-        headlines[i] = headlines[i].text
-    
-    for i in range(0, len(bodies)):
-        bodies[i] = bodies[i].text
-    
-    category = [category]* len(headlines)
-    
-    #create a list of dictionaries:
-    list = []
-    for i in range (0, len(headlines)):
-        list.append({'title': headlines[i], 'content': bodies[i], 'category': category[i]})
+        headers = {'User-Agent': 'Codeup Data Science'}
+        response = get(url, headers=headers)
+        soup = BeautifulSoup(response.text)
+        headlines = soup.find_all(attrs={"itemprop": "headline"})
+        bodies = soup.find_all(attrs={"itemprop": "articleBody"})
+        category = re.search(r'([^\/]+$)', url)[0]
+        
+        #get list of headlines, content and categories
+        for i in range(0, len(headlines)):
+            headlines[i] = headlines[i].text
+            
+        for i in range(0, len(bodies)):
+            bodies[i] = bodies[i].text
+        
+        category = [category]* len(headlines)
+        
+        #create a list of dictionaries:
+        for i in range (0, len(headlines)):
+            headline = headlines[i]
+            body = bodies[i]
+            cat = category[i]
+            dict = ({'topic': cat, 
+                                'title': headline, 
+                                'content': body})
+            list.append(dict)
+       
     return list
